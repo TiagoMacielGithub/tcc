046b0dc6c338539aa3254aa5daa7c6abadecb425
unknown <35177@fiecalunos.lan>
2023-07-25 21:55:15 -0300

    teste

--- src/models/entities/Repositories.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UsuarioRepositorio = void 0;
-const data_source_1 = require("../../data-source");
-const Usuario_1 = __importDefault(require("./Usuario"));
-exports.UsuarioRepositorio = data_source_1.AppDataSource.getRepository(Usuario_1.default);
